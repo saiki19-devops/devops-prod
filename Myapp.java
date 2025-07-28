86ae5df7a71600fff94c8f4c2f56db6634834ba3
Kiran <masurasaikiran>
2025-07-28 07:02:55 +0000

    add-sub-by-kiran

--- Myapp.java
+++ Myapp.java
@@ -2,6 +2,9 @@ public class Msg{
 	public static void main(string[] args){
 		int a=10, int b=20;
 		System.out.println("Sum is:",+(a+b));
+                System.out.println("differenceis:",+(a-b));
+
+
 	}
 }
 
