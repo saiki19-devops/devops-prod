0ba2609762603df630a6843834bccce99fd6b54f
saiki19-devops <masurasaikiran@gmail.com>
2025-08-02 22:59:42 +1000

    Update Myapp.java

--- Myapp.java
+++ Myapp.java
@@ -1,10 +1,8 @@
-public class Msg{
-	public static void main(string[] args){
-		int a=10,  b=20;
-		System.out.println("Sum is:",+(a+b));
-                System.out.println("differenceis:",+(a-b));
-
-
-	}
+public class Myapp {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Sum is: " + (a + b));
+        System.out.println("Difference is: " + (a - b));
+    }
 }
 
